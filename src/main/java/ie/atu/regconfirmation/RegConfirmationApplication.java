b8860bfef9c42d38d15de2e960b7c618005ba045
dewanhasan99 <g00410156@atu.ie>
2023-12-27 14:24:29 +0000

    First Commit

---
+++ src/main/java/ie/atu/regconfirmation/RegConfirmationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.regconfirmation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegConfirmationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegConfirmationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/regconfirmation/RegConfirmationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.regconfirmation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegConfirmationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
